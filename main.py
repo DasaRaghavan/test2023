014471506646ac0bea98ebb2f75ae74024ddd581
dasar <dashfamily@gmail.com>
2023-12-27 14:17:18 -0500

    intiial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+lower_bound = int(input("Enter a lower bound: "))
+upper_bound = int(input("Enter a upper bound: "))
+
+my_random_output = random.randint(lower_bound, upper_bound)
+
+print(my_random_output)
+
